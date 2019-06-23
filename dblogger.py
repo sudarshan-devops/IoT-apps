b9388f0a9a9e4ff93d446581c16722c786cb5411
sudarshan <sudarshandevarasam@gmail.com>
2019-06-23 14:12:56 +0000

    updated mysql-connector

--- dblogger.py
+++ dblogger.py
@@ -1,29 +1,31 @@
 #!/usr/bin/python
-import MySQLdb
+import mysql.connector
 import sys
 import Adafruit_DHT
-import datatime
+import datetime
 from time import sleep
-db=MySQLdb.connect("localhost","root","12345","testdb")
+db=mysql.connector.connect(user="user",password="passw0rd",host="localhost",database="db1")
 cursor = db.cursor()
-createtale()
-count=0
+table=raw_input("enter the name of the table you wish to create")
+
 def dht11():
+    count=0
     ch=int(input("enter the logging duration in Seconds"))
     while(count<=ch):
-        x = datetime.datetime.now()
-        humidity, temperature = Adafruit_DHT.read_retry(11, 21)
-        print(temperature, humidity)
-        query="insert into "+table+" values("+x+","+temperature+","+humidity+")"
+        x = datetime.datetime.now().time()
+        humidity, temperature = Adafruit_DHT.read_retry(11, 14)
+        print("temperature=%f humidity=%f"%(temperature,humidity)) 
+        query="insert into "+table+" values("+str(x)+","+str(temperature)+","+str(humidity)+")"
         cursor.execute(query)
         sleep(1)
         count=count+1
 
 def createtable():
-    table=input("enter the name of table you wish to create")
     cursor.execute("drop table if exists "+table)
     sql="create table " +table+ "(DateTime varchar(15),temperature varchar(5),humidity varchar(5))"
     cursor.execute(sql)
+    dht11()
 
+createtable()
 db.close()
 
